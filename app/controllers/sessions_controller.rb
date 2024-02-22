e068d05c2e38fff2ae9547abcf87320039ad32e8
uma vadlamani <umasai.vadlamani@kellogg.northwestern.edu>
2024-02-22 03:29:08 +0000

    Idk what's going on

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,32 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
-    # 2. if the user exists -> check if they know their password
-    # 3. if they know their password -> login is successful
-    # 4. if the user doesn't exist or they don't know their password -> login fails
-    flash["notice"] = "Welcome."
-    redirect_to "/companies"
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil 
+      # 2. if the user exists -> check if they know their password
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+      # 3. if they know their password -> login is successful
+      # 4. if the user doesn't exist or they don't know their password -> login fails
+       
+      # add a cookie for this user 
+      cookies["monster"] = "me like cookies"
+      session["user_id"]= @user["id"]
+      
+        flash["notice"] = "Welcome."
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,11 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "puffies"
+encrypted = BCrypt::Password.create(secret)
+puts encrypted
 # 2. prepare encrypted string for testing
+user_entered_secret = "puffied"
+result = BCrypt::Password.new(encrypted) == user_entered_secret
+puts result
 # 3. test secret against prepared encrypted string
